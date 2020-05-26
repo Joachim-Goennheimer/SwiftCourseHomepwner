a86f3d8a5efc55a340ac99aa4a47bf8b23f0cdef
Joachim Goennheimer <Joachim.goennheimer@ibm.com>
2020-05-26 09:33:25 +0200

    implement segue prepare function

--- Homepwner/itemsViewController.swift
+++ Homepwner/itemsViewController.swift
@@ -173,5 +173,19 @@ class ItemsViewController: UITableViewController {
             cell.valueLabel.textColor = UIColor.green
         }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "showItem"?:
+            
+            if let row = tableView.indexPathForSelectedRow?.row {
+                let item = itemStore.allItems[row]
+                let detailViewController = segue.destination as! DetailViewController
+                detailViewController.item = item
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier")
+        }
+    }
 }
 
